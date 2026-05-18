302505106e42c1bfa1290ee477337aa320bf8927
Corey Murphy <corey.murphy@seebyte.us>
2026-05-18 15:07:16 -0700

    Add FitToLocations component to adjust map view based on tile locations

--- frontend/src/components/LiveMap.tsx
+++ frontend/src/components/LiveMap.tsx
@@ -1,4 +1,5 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import type { TileLocation } from "../hooks/useTileLocations";
 
 type LiveMapProps = {
@@ -7,10 +8,40 @@ type LiveMapProps = {
 
 const DEFAULT_CENTER: [number, number] = [38.076, -81.073];
 
+type FitToLocationsProps = {
+  locations: TileLocation[];
+};
+
+function FitToLocations({ locations }: FitToLocationsProps) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (locations.length === 0) {
+      return;
+    }
+
+    if (locations.length === 1) {
+      const onlyTile = locations[0];
+      map.setView([onlyTile.latitude, onlyTile.longitude], 19, { animate: false });
+      return;
+    }
+
+    const bounds: [number, number][] = locations.map((item) => [item.latitude, item.longitude]);
+    map.fitBounds(bounds, {
+      animate: false,
+      maxZoom: 19,
+      padding: [28, 28],
+    });
+  }, [locations, map]);
+
+  return null;
+}
+
 export function LiveMap({ locations }: LiveMapProps) {
   return (
     <section className="map-frame">
       <MapContainer center={DEFAULT_CENTER} zoom={14} className="map-canvas">
+        <FitToLocations locations={locations} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
